03175f4f90420c6e397ebb3f79c4194f1bbdeef4
Niko Noll <dernikonoll@gmail.com>
2016-04-23 20:29:14 +0200

    fixed for loop

--- LEDparty.py
+++ LEDparty.py
@@ -6,13 +6,13 @@ GPIO.setmode(GPIO.BOARD)
 
 ControlPin = [29,31,33,35]
 
-for i in ControlPin: 
-	GPIO.setup(i,GPIO.OUT)
-	GPIO.output(i,0)
+for pin in ControlPin:
+	GPIO.setup(pin,GPIO.OUT)
+	GPIO.output(pin,0)
 
 seq = [[1,0,0,0], [0,1,0,0], [0,0,1,0], [0,0,0,1]]
 
-for x in range(4)
+for x in range(4):
 	for y in range(4):
 			GPIO.output(ControlPin[y],seq[x][y])
 		time.sleep(1)
