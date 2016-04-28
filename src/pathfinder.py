0797baf231ce1c101830f1a570ecec15f595a980
LukasOtis <lukasotisnoll@googlemail.com>
2016-04-28 16:44:18 +0000

    trial and error with the raspberry and pathfinder, it was nice

--- src/pathfinder.py
+++ src/pathfinder.py
@@ -1,6 +1,6 @@
-# import RPi.GPIO as GPIO
-from test.devStubs import *
-import time as sleep
+import RPi.GPIO as GPIO
+#from test.devStubs import *
+from time import sleep
 
 time_1 = 0.5
 time_2 = 0.25
@@ -26,15 +26,17 @@ def step_one():
     GPIO.output(pinD, 0)
     GPIO.output(pinB, 0)
     sleep(time_1)
-
+    print('done with step one')
 
 def step_two():
+    global time_1
+    global time_2    
     GPIO.output(pinB, 1)
     sleep(time_2)
     GPIO.output(pinA, 0)
     GPIO.output(pinC, 0)
     sleep(time_1)
-
+    print('done with step two')
 
 def step_three():
     GPIO.output(pinC, 1)
@@ -58,49 +60,62 @@ horizontal_min_value = -horizontal_max_value
 
 # create start state
 x_position = 0
-rotation_position = 0
+rotation_position = 1
 
 
-def move(steps):
+def move(steps, rotation_position, x_position):
+    print('trying to move', steps)
     if(steps == 0):
         print('nothing changed')
 
-    boundsFlag = x_position + steps
-    if(boundsFlag > horizontal_max_value | boundsFlag < horizontal_min_value):
-        print('error: command out of bounds')
+    #boundsFlag = x_position + steps
+    #if(boundsFlag > horizontal_max_value | boundsFlag < horizontal_min_value):
+    #    print('error: command out of bounds')
     if(steps < 0):
-        rotate_backwards(steps)
+        rotate_backwards(steps, rotation_position, x_position)
     else:
-        rotate_forwards(steps)
-
+        return rotate_forwards(steps, rotation_position, x_position)
+ 
 
 # move forward a given amount of quarters
-def rotate_forwards(steps):
-    for step in steps:
+def rotate_forwards(steps, rotation_position, x_position):
+    print('rotating forwards')
+    for step in range(steps):
+        print('i´m at step', step)
         if (rotation_position == 1):
-            step_two
-            rotation_position = 2
+            step_two()
+            #rotation_position = rotation_position + 1
             x_position = x_position + 1
 
         if (rotation_position == 2):
-            step_three
-            rotation_position = 3
+            step_three()
+            #rotation_position = rotation_position + 1
             x_position = x_position + 1
 
         if (rotation_position == 3):
-            step_four
-            rotation_position = 4
+            step_four()
+            #rotation_position = rotation_position + 1
             x_position = x_position + 1
 
         if (rotation_position == 4):
-            step_one
-            rotation_position = 1
+            step_one()
+            #rotation_position = rotation_position + 1
             x_position = x_position + 1
-
+        
+    
+        rotation_position += 1
+    
+        if rotation_position == 5:
+            rotation_position = 1
+ 
+    return(rotation_position)
+        
 
 # moe backwards a given amount of quaters
-def rotate_backwards(steps):
-    for step in steps:
+def rotate_backwards(steps, rotation_position, x_position):
+    print('rotate backwards')
+    for step in range(steps):
+        print('i´m at step', step)
         if (rotation_position == 1):
             step_four
             rotation_position = 4
@@ -123,8 +138,10 @@ def rotate_backwards(steps):
 
 
 '''main method'''
-def main():
-    while(true):
-        steps = int(input("How far do you want to walk?: "))
-        move(steps)
-        print('Done with steps --> New x_position: ' + x_position)
+while(True):
+    steps = input("How far do you want to walk?: ")
+    steps = int(steps)
+    response = move(steps, rotation_position, x_position)
+    print('Done with steps --> New x_position: ', x_position)
+    print('new rotation position', response)
+    rotation_position = response
