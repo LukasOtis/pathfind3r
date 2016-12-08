d0680ce017ca59a3ac92d6fa1e4dfbebc29fe89d
LukasOtis <lukasotisnoll@googlemail.com>
2016-12-08 19:37:40 +0100

    inplemented halfstepping

--- src/motor_controller.py
+++ src/motor_controller.py
@@ -15,120 +15,93 @@ class MotorController:
     def motor_sleep(self):
         time.sleep(self.sleep_time)
 
-    def rotate_forwards(self, steps, rotation_position):
+    def rotate_forwards(self, rotation_position):
         """Move forward a given amount of quarters."""
 
         if rotation_position == 0:
             self.motor.step_one()
             rotation_position += 1
-            halt
+            motor_sleep
 
         elif rotation_position == 1:
             self.motor.step_two()
             rotation_position += 1
-            halt
+            motor_sleep
 
         elif rotation_position == 2:
             self.motor.step_three()
             rotation_position += 1
-            halt
+            motor_sleep
 
         elif rotation_position == 3:
             self.motor.step_four()
             rotation_position += 1
-            halt
+            motor_sleep
 
         elif rotation_position == 4:
             self.motor.step_five()
             rotation_position += 1
-            halt
+            motor_sleep
 
         elif rotation_position == 5:
             self.motor.step_six()
             rotation_position += 1
-            halt
+            motor_sleep
 
         elif rotation_position == 6:
             self.motor.step_seven()
             rotation_position += 1
-            halt
+            motor_sleep
 
         else:
             self.motor.step_zero()
             rotation_position = 0
-            halt
+            motor_sleep
 
         return rotation_position
 
-    def rotate_backwards(self, steps, rotation_position):
+    def rotate_backwards(self, rotation_position):
         """Move backwards a given amount of quarters."""
 
         if rotation_position == 0:
-            self.motor.step_two()
-            rotation_position += 1
-            halt
+            self.motor.step_seven()
+            rotation_position == 7
+            motor_sleep
 
         elif rotation_position == 1:
-            self.motor.step_three()
-            rotation_position += 1
-            halt
+            self.motor.step_zero()
+            rotation_position -= 1
+            motor_sleep
 
         elif rotation_position == 2:
-            self.motor.step_four()
-            rotation_position += 1
-            halt
+            self.motor.step_one()
+            rotation_position -= 1
+            motor_sleep
 
         elif rotation_position == 3:
-            self.motor.step_five()
-            rotation_position += 1
-            halt
+            self.motor.step_two()
+            rotation_position -= 1
+            motor_sleep
 
         elif rotation_position == 4:
-            self.motor.step_six()
-            rotation_position += 1
-            halt
+            self.motor.step_three()
+            rotation_position -= 1
+            motor_sleep
 
         elif rotation_position == 5:
-            self.motor.step_seven()
-            rotation_position += 1
-            halt
+            self.motor.step_four()
+            rotation_position -= 1
+            motor_sleep
 
         elif rotation_position == 6:
-            self.motor.step_eight()
-            rotation_position += 1
-            halt
+            self.motor.step_five()
+            rotation_position -= 1
+            motor_sleep
 
         else:
-            self.motor.step_one()
-            rotation_position = 0
-            halt
-
-        return rotation_position
-
-    def rotate_backwards(self, steps, rotation_position):
-        """Move backward a given amount of quarters."""
-        for step in range(steps * (-1)):
-
-            if rotation_position == 0:
-                self.motor.step_four()
-                time.sleep(0.01)
-
-            if rotation_position == 1:
-                self.motor.step_one()
-                time.sleep(0.01)
-
-            if rotation_position == 2:
-                self.motor.step_two()
-                time.sleep(0.01)
-
-            if rotation_position == 3:
-                self.motor.step_three()
-                time.sleep(0.01)
-
-            rotation_position -= 1
-
-            if rotation_position == -1:
-                rotation_position = 3
+            self.motor.step_six()
+            rotation_position -=0
+            motor_sleep
 
         return rotation_position
 
