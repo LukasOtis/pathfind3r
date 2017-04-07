3ebc078bd77f41defad2a0c01b9064158c80f12d
LukasOtis <lukasotisnoll@googlemail.com>
2017-04-07 17:56:05 +0200

    another update
    some minor difficulties left

--- src/gcodeparser.py
+++ src/gcodeparser.py
@@ -1,40 +1,34 @@
 import csv
-
 class GCodeParser:
-    def __init__(filename):
-        file = open('filename')
+    @staticmethod
+    def OpenFile(filename):
+        file = open(filename)
         reader = csv.reader(file)
         data = list(reader)
-        length=len(data)
-        return GCodeParser(length)
+        length = len(data)
+        return data
 
     @staticmethod
-    def fromLine(line):
-        if data(line) == '':
-            return None
+    def FromLine(data, line):
+        row = data[line]
+        value = (row[0])
+        coordinates = []
 
-        else
-            row = data[line]
-            value = (row[0])
-            xcoord = float(0)
-            ycoord = float(0)
-            zcoord = float(0)
+        # Extract X coordinate
+        index_one = value.index(';')
+        xcoord = value[0:index_one]
 
-            # Extract X coordinate
-            index_one = value.index(';')
-            x_value = value[0:index_one]
-            xcoord = float(x_value)
+        # Extract Y coordinate
+        temp = value[index_one+1:len(value)]
+        index_two = temp.index(';')
+        ycoord = temp[0:index_two]
 
-            # Extract Y coordinate
-            temp = value[index_one+1:len(value)]
-            index_two = temp.index(';')
-            y_value = temp[0:index_two]
-            ycoord = float(y_value)
+        # Extract Z coordinate
+        zcoord = temp[index_two+1:len(temp)]
 
-            # Extract Z coordinate
-            z_value = temp[index_two+1:len(temp)]
-            zcoord = float(z_value)
-            
-            coords=list(xcoord, ycoord, zcoord)
-        return GCodeParser(coords)
-
+        # return Coordinates
+        coordinates.append(xcoord)
+        coordinates.append(ycoord)
+        coordinates.append(zcoord)
+        return (coordinates)
+        
